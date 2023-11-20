d87279606f0713e9b43b1e82b6d18517ea978598
Convene9340 <145895237+Convene9340@users.noreply.github.com>
2023-11-20 23:39:42 +0800

    Add base html and script

---
+++ script.js
@@ -0,0 +1,24 @@
+/*
+MODEL
+    Objects:
+    gameBoard
+        methods:
+            update
+            validMove
+    player
+        instances: player1 and player2
+        methods: move
+
+        properties:
+        score
+
+CONTROLLER
+    game
+        start
+        move(player, coordinate)
+
+VIEW
+    html
+
+
+*/
