80c0e857ff4cdf28c3ec3e7062c7178d3edcbd59
Convene9340 <145895237+Convene9340@users.noreply.github.com>
2023-11-21 21:56:41 +0800

    Add board object

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 MODEL
     Objects:
     gameBoard
+
+    [
+        [0,0,0],
+        [0,0,o],
+        [0,0,x]
+    ]
         methods:
             update
             validMove
@@ -22,3 +28,27 @@ VIEW
 
 
 */
+
+function gameBoard(){
+
+    var board = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ]
+    //pieces
+
+    const update = (coordinate, piece) => {
+        
+        const [row, column] = coordinate
+        
+        if(board[row][column] ===0){
+            board[row][column]=piece
+        }
+        
+        
+        
+    }
+
+    return {board, update}
+}
